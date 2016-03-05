56d4d4f4bce64d59162cb623fdb06eee4bc8bfe4
Nishanth Sivakumar <nishanth@iastate.edu>
2016-03-05 17:47:22 -0600

    updated code for min hash

--- src/minhash/MinHash.java
+++ src/minhash/MinHash.java
@@ -1,11 +1,9 @@
 package minhash;
 
+import objects.Permutation;
 import utilities.FileUtil;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.logging.Logger;
 
 /**
@@ -21,8 +19,9 @@ public class MinHash {
     private String[] filenames;
     private int[][] termDocumentMatrix;
     private int[][] minHashMatrix;
-    private Set<String> termSet;
+    private List<String> termList;
     private HashMap<String, Set<String>> termMap;
+    private Permutation[] permutations;
 
     public int getNumPermutations() {
         return numPermutations;
@@ -32,11 +31,22 @@ public class MinHash {
         this.numPermutations = numPermutations;
     }
 
+    public int numTerms(){
+        return this.termList.size();
+    }
+
+    public int[][] minHashMatrix(){
+        return this.minHashMatrix;
+    }
+
     public MinHash(String folder, int numPermutations){
         this.folder = folder;
         this.numPermutations = numPermutations;
         this.filenames = getAllFileNames();
         this.termMap = new HashMap<>();
+        this.termList = new ArrayList<>();
+        this.permutations = new Permutation[numPermutations];
+        this.minHashMatrix = new int[numPermutations][filenames.length];
         computeMinHashSig();
     }
 
@@ -59,31 +69,96 @@ public class MinHash {
         return filenames;
     }
 
-    private Set<String> getTermIntersection(Set<String> termSet1, Set<String> termSet2){
-        final String METHOD_NAME = "getTermIntersection";
-        LOGGER.entering(CLASS_NAME, METHOD_NAME);
-
-        Set<String> termSet = new TreeSet<String>();
-        Iterator<String> file1TermIterator = termSet1.iterator();
-        while(file1TermIterator.hasNext()){
-            String term = file1TermIterator.next();
-            if(termSet2.contains(term)){
-                termSet.add(term);
-            }
-        }
-        LOGGER.exiting(CLASS_NAME, METHOD_NAME);
-        return termSet;
-    }
-
     private void computeMinHashSig(){
         final String METHOD_NAME = "computeMinHashSig";
         LOGGER.entering(CLASS_NAME, METHOD_NAME);
 
         createTermSet();
         createTermDocumentMatrix();
+        generatePermutations();
+        //compute minhashsignature
+        for(int i=0;i<this.numPermutations; i++){
+            Permutation currentPerm = permutations[i];
+            for(int j=0;i<filenames.length;j++){
+               minHashMatrix[i][j] = getMinimumTerm(getTermDocumentColumn(j),currentPerm).hashCode();
+            }
+        }
+        LOGGER.exiting(CLASS_NAME, METHOD_NAME);
+    }
 
+    private int getFileIndex(String filename){
+        final String METHOD_NAME = "getFileIndex";
+        LOGGER.entering(CLASS_NAME, METHOD_NAME);
+
+        int columnIndex = -1;
+        for(int i=0;i<filenames.length;i++) {
+            if (filenames[i].equals(filename)) {
+                columnIndex = i;
+                break;
+            }
+        }
 
         LOGGER.exiting(CLASS_NAME, METHOD_NAME);
+        return columnIndex;
+    }
+
+    public int[] minHashSig(String filename){
+        final String METHOD_NAME = "minHashSig";
+        LOGGER.entering(CLASS_NAME, METHOD_NAME);
+
+        int columnIndex = getFileIndex(filename);
+        int[] docMinHashSig = new int[this.numPermutations];
+
+        if(columnIndex != -1){
+            for(int i=0;i<this.numPermutations;i++){
+                docMinHashSig[i] = this.minHashMatrix[i][columnIndex];
+            }
+        }
+
+        LOGGER.exiting(CLASS_NAME, METHOD_NAME);
+        return docMinHashSig;
+    }
+
+    private int[] getTermDocumentColumn(int columnNumber){
+        final String METHOD_NAME = "getTermDocumentColumn";
+        LOGGER.entering(CLASS_NAME, METHOD_NAME);
+
+        int[] termDocColumn = new int[this.termList.size()];
+        for(int i=0;i<termList.size();i++){
+            termDocColumn[i] = termDocumentMatrix[i][columnNumber];
+        }
+
+        LOGGER.exiting(CLASS_NAME, METHOD_NAME);
+        return termDocColumn;
+    }
+    private void generatePermutations(){
+        final String METHOD_NAME = "generatePermutations";
+        LOGGER.entering(CLASS_NAME, METHOD_NAME);
+
+        int a = 1, b=3;
+        for(int i=0;i< this.numPermutations;i++){
+            permutations[i] = new Permutation(a,b);
+            a +=1;
+            b+=b;
+        }
+        LOGGER.exiting(CLASS_NAME, METHOD_NAME);
+    }
+    private String getMinimumTerm(int[] termDocument, Permutation p){
+        final String METHOD_NAME = "getMinimumTerm";
+        LOGGER.entering(CLASS_NAME, METHOD_NAME);
+
+        List<String> occurringTerms = new ArrayList<>();
+
+        for(int i=0;i<termDocument.length;i++){
+            if(termDocument[i] == 1){
+                int index = ((p.getA()+i)+p.getB())%this.termList.size();
+                occurringTerms.add(this.termList.get(index));
+            }
+        }
+        Collections.sort(occurringTerms);
+
+        LOGGER.exiting(CLASS_NAME, METHOD_NAME);
+        return occurringTerms.get(0);
     }
 
     private void createTermSet(){
@@ -91,13 +166,14 @@ public class MinHash {
         LOGGER.entering(CLASS_NAME, METHOD_NAME);
 
         String [] filenames = allDocs();
+        Set<String> localTermSet = new TreeSet<>();
         for(String file: filenames){
             String filepath = folder.endsWith("/") ? (folder + file) : (folder + "/" + file);
             Set<String> fileTerms = FileUtil.getFileTerms(filepath);
             this.termMap.put(file, fileTerms);
-            termSet.addAll(fileTerms);
+            localTermSet.addAll(fileTerms);
         }
-
+        this.termList.addAll(localTermSet);
         LOGGER.exiting(CLASS_NAME, METHOD_NAME);
     }
 
@@ -105,10 +181,9 @@ public class MinHash {
         final String METHOD_NAME = "createTermDocumentMatrix";
         LOGGER.entering(CLASS_NAME, METHOD_NAME);
 
-        this.termDocumentMatrix = new int[termSet.size()][filenames.length];
-        Iterator<String> termSetIterator = termSet.iterator();
-        for(int i=0;i<termSet.size() && termSetIterator.hasNext();i++){
-            String term = termSetIterator.next();
+        this.termDocumentMatrix = new int[termList.size()][filenames.length];
+        for(int i = 0; i< termList.size(); i++){
+            String term = this.termList.get(i);
             for(int j=0;j<filenames.length;j++){
                 String filename = filenames[j];
                 if(this.termMap.get(filename).contains(term)){
@@ -118,27 +193,63 @@ public class MinHash {
                 }
             }
         }
-
         LOGGER.exiting(CLASS_NAME, METHOD_NAME);
     }
+
+    private Set<String> getTermIntersection(Set<String> termSet1, Set<String> termSet2){
+        final String METHOD_NAME = "getTermIntersection";
+        LOGGER.entering(CLASS_NAME, METHOD_NAME);
+
+        Set<String> intersectionSet = new TreeSet<>();
+        Iterator<String> file1TermIterator = termSet1.iterator();
+        while(file1TermIterator.hasNext()){
+            String term = file1TermIterator.next();
+            if(termSet2.contains(term)){
+                intersectionSet.add(term);
+            }
+        }
+        LOGGER.exiting(CLASS_NAME, METHOD_NAME);
+        return intersectionSet;
+    }
+
     public double exactJaccard(String file1, String file2){
         final String METHOD_NAME = "exactJaccard";
         LOGGER.entering(CLASS_NAME, METHOD_NAME);
 
         double jaccardSim = 0.0;
-        String file1path = folder.endsWith("/") ? (folder + file1) : (folder + "/" + file1);
-        String file2path = folder.endsWith("/") ? (folder + file2) : (folder + "/" + file2);
-
-        Set<String> file1Terms = FileUtil.getFileTerms(file1path);
-        Set<String> file2Terms = FileUtil.getFileTerms(file2path);
+        Set<String> file1Terms = this.termMap.get(file1);
+        Set<String> file2Terms = this.termMap.get(file2);
 
         Set<String> termSet = getTermIntersection(file1Terms, file2Terms);
 
-        file1Terms.addAll(file2Terms);
-        jaccardSim = termSet.size()/(file1Terms.size());
+        Set<String> unionSet = new TreeSet<>();
+        unionSet.addAll(file1Terms);
+        unionSet.addAll(file2Terms);
+        jaccardSim = termSet.size()/(unionSet.size());
 
         LOGGER.exiting(CLASS_NAME, METHOD_NAME);
         return jaccardSim;
+    }
+
+    public double approximateJaccard(String file1, String file2){
+        final String METHOD_NAME = "approximateJaccard";
+        LOGGER.entering(CLASS_NAME, METHOD_NAME);
+
+        int[] file1MinHash = minHashSig(file1);
+        int[] file2MinHash = minHashSig(file2);
+
+        int matchingCount = 0;
+
+        for(int i=0;i<file1MinHash.length;i++){
+            for(int j=0;j<file2MinHash.length;j++){
+                if(file1MinHash[i] == file2MinHash[j]){
+                    matchingCount++;
+                }
+            }
+        }
+
+        LOGGER.exiting(CLASS_NAME, METHOD_NAME);
+        return matchingCount/file1MinHash.length;
     }
 
 
---
+++ src/objects/Permutation.java
@@ -0,0 +1,32 @@
+package objects;
+
+/**
+ * Created by Nishanth Sivakumar and Sriram Balasubramanian on 3/5/16.
+ */
+public class Permutation {
+
+    private int a;
+    private int b;
+
+    public Permutation(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+}
