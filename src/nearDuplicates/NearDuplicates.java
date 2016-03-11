6abea299347391cf0188aa717488fe039daa776d
Sriram Balasubramanian <sriramb@iastate.edu>
2016-03-10 22:24:58 -0600

    Added false positives to near duplicates

--- src/nearDuplicates/NearDuplicates.java
+++ src/nearDuplicates/NearDuplicates.java
@@ -1,5 +1,10 @@
 package nearDuplicates;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 import lsh.LSH;
 import minhash.MinHash;
 
@@ -13,18 +18,28 @@ public class NearDuplicates {
 		minHash.computeMinHashSig();
 		LSH lsh = new LSH(minHash.minHashMatrix(), minHash.allDocs(), numBands);
 		String[] nearSimilarFile = lsh.nearDuplicatesOf(docName);
-		for ( String file : nearSimilarFile) {
-			System.out.println(file + " - " + minHash.approximateJaccard(file, docName));
+		List<String> files = new ArrayList<String>(Arrays.asList(nearSimilarFile));
+		Iterator<String> itr = files.iterator();
+		int falsePostives = 0;
+		while (itr.hasNext()){
+			if(minHash.approximateJaccard(itr.next(), docName) < similarityThreshold) {
+				falsePostives++;
+				itr.remove();
+			}
 		}
-		return nearSimilarFile;
+		System.out.println("False postives - " + falsePostives);
+		return files.toArray(new String[0]);
 	}
 	
 	public static void main(String[] args){
 		String docFolder = "C://Users//Sriram//Desktop//Study//535//PA2//pa2-partial//";
 		Integer numPermutations = 300;
 		Integer numBands = 50;
-		Float similarityThreshold = 0.9f;
+		Float similarityThreshold = 0.8f;
 		String docName = "space-0.txt";
-		sSimilarDocuments(docFolder, numPermutations, numBands, similarityThreshold, docName);
+		String[] nearSimilarFile = sSimilarDocuments(docFolder, numPermutations, numBands, similarityThreshold, docName);
+		for ( String file : nearSimilarFile) {
+			System.out.println(file);
+		}
 	}
 }
